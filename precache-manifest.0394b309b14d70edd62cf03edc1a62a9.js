d440c73ffa5397696427d16820cccbec18f640a6
Sergei <sergei.rizzotti@gmail.com>
2020-05-24 15:53:38 +0300

    Updates

---
+++ precache-manifest.0394b309b14d70edd62cf03edc1a62a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57168d9e18bed75973def0180cd003d",
+    "url": "/learnEnglish/index.html"
+  },
+  {
+    "revision": "c3679b51592aa515db20",
+    "url": "/learnEnglish/static/css/main.195a0444.chunk.css"
+  },
+  {
+    "revision": "55b1ab55732774cd0925",
+    "url": "/learnEnglish/static/js/2.88b96ba4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learnEnglish/static/js/2.88b96ba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3679b51592aa515db20",
+    "url": "/learnEnglish/static/js/main.29407bb4.chunk.js"
+  },
+  {
+    "revision": "558d4e2891e29167a90f",
+    "url": "/learnEnglish/static/js/runtime-main.2d8e14a4.js"
+  },
+  {
+    "revision": "afc3ac95e8b6bef118007d755af3d21e",
+    "url": "/learnEnglish/static/media/arrows-down.afc3ac95.svg"
+  },
+  {
+    "revision": "898d65d07b3ff005f2afa1f917b5f6da",
+    "url": "/learnEnglish/static/media/background.898d65d0.jpg"
+  }
+]);
